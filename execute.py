e6ee95bcf9d5b12e8457ab3fc796aaf3df975047
axhue <qiahmedmail@gmail.com>
2018-03-15 16:08:00 -0400

    cleanup

--- execute.py
+++ execute.py
@@ -1,8 +1,11 @@
-from agent import Agent,AgentConfig
+from code.agent import Agent,AgentConfig
 import gym
-from preprocessors import AtariPreprocessor
-from policies import LinearDecayGreedyEpsilonPolicy
+from code.preprocessors import AtariPreprocessor
+from code.policies import LinearDecayGreedyEpsilonPolicy
 import os
+from keras import backend as K
+import tensorflow as tf
+import numpy as np
 
 
 def _test(env,episodes):
@@ -66,14 +69,20 @@ def get_output_folder(args, parent_dir, env_name, task_name):
 
 if __name__== '__main__':
     atari_env = gym.make('Seaquest-v0')
-    doc = get_output_folder(None,"./logs","Seaquest-v0","DQRN-10")
-
-    episodes = 1000000
+    doc = get_output_folder(None,"./logs","Seaquest-v0","DQRN-19")
+    #np.random.seed(0)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.allow_soft_placement = True
+    sess = tf.Session(config=config)
+    K.tensorflow_backend.set_session(sess)
+    
+    episodes = 100000
     cfg = AgentConfig()
     cfg.stateCnt = atari_env.observation_space.shape
     cfg.actionCnt = atari_env.action_space.n
     cfg.mem_size = 1000000
-    cfg.epsilon_policy = LinearDecayGreedyEpsilonPolicy(1.0,0.05,episodes)
+    cfg.epsilon_policy = LinearDecayGreedyEpsilonPolicy(1.0,0.05,episodes+200000 )
     cfg.gamma = 0.99
     cfg.num_frames = 10
     cfg.learning_rate = 0.0001
@@ -83,6 +92,7 @@ if __name__== '__main__':
     cfg.batch_size = 32
     cfg.preprocessor = AtariPreprocessor((84,84,1))
     cfg.log_path = doc
-    cfg.name = "R7"
-    agent = Agent(cfg)
+    cfg.name = "R19"
+    agent = Agent(cfg,sess)
     agent.train(atari_env,episodes,render=False)
+    #agent.fit(atari_env,episodes)
